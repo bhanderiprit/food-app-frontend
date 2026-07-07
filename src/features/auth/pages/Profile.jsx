ece69fdea35643e98ac2d859a7cf2c733ca5bf49
Prit <bhanderiprit3730@gmail.com>
2026-07-08 00:07:16 +0530

    Fix profile import

--- src/features/auth/pages/Profile.jsx
+++ src/features/auth/pages/Profile.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 import "../../../css/profile.css";
-import BottamNavbar from "../components/bottamNavbar";
+import BottamNavbar from "../components/BottamNavbar";
 
 const Profile = () => {
   const { id } = useParams();
